cb98ecff0fc8c9fb04e638e1bd3c41068ef62657
limyt0 <80087763+limyt0@users.noreply.github.com>
2024-09-27 02:58:01 +0900

    Update a07정수론43_p1850_최대공약수.java

--- JavaAlgo/a07/a07정수론43_p1850_최대공약수.java
+++ JavaAlgo/a07/a07정수론43_p1850_최대공약수.java
@@ -6,20 +6,22 @@ import java.util.*;
 public class a07정수론43_p1850_최대공약수 {
 
 	public static void main(String[] args) {
+	
 		Scanner sc = new Scanner(System.in);
 		long a = sc.nextLong();
 		long b = sc.nextLong();
 		long result = LongGDC(a, b);
-		//System.out.println("result:"+result);
+		
+		StringBuilder sb = new StringBuilder();
 		while(result > 0) {
-			System.out.print("1");
+			sb.append("1");
 			result--;
 		}
+		System.out.println(sb);
 
 	}
 
 	private static long LongGDC(long a, long b) {
-		//System.out.println(a+", "+b);
 		if(b== 0) {
 			return a;
 		}else {
