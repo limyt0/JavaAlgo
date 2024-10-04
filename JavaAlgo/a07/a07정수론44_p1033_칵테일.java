db92a5e8c469f0779ddf3068f40e4a4179cac0be
limyt0 <80087763+limyt0@users.noreply.github.com>
2024-10-04 23:17:46 +0900

    Update a07정수론44_p1033_칵테일.java

--- JavaAlgo/a07/a07정수론44_p1033_칵테일.java
+++ JavaAlgo/a07/a07정수론44_p1033_칵테일.java
@@ -1,10 +1,28 @@
 package a07;
 
+import java.io.*;
+import java.util.*;
+
 public class a07정수론44_p1033_칵테일 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n;//재료 수
+	static int[] a;//인접리스트
+	static int lcm; //최소공배수
+	static int[] d;//각 노드 저장 배열
+	boolean[] visited;//방문여부 저장 배열
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		a = new int[n];
+		for(int i = 0;i<n-1;i++) {
+			st = new StringTokenizer(br.readLine());
+			a[i] = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			d[i] = Integer.parseInt(st.nextToken());
+			
+		}
+		
 	}
 
 }
