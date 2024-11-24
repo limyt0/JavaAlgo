235d23ea58b862198fcf3c91b9932e490352b852
limyt0 <dudx45@gmail.com>
2024-11-24 23:23:10 +0900

    p2023문제 다시 풀기

--- JavaAlgo/a05/a5탐색24_p2023신기한소수.java
+++ JavaAlgo/a05/a5탐색24_p2023신기한소수.java
@@ -4,12 +4,13 @@ import java.io.*;
 import java.util.*;
 
 public class a5탐색24_p2023신기한소수 {
-	static int n;
+	static int n ;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		n = Integer.parseInt(st.nextToken());
-		//첫자리는 무조건 2, 3, 5, 7
+		
 		dfs(2, 1);
 		dfs(3, 1);
 		dfs(5, 1);
@@ -18,34 +19,28 @@ public class a5탐색24_p2023신기한소수 {
 
 	private static void dfs(int num, int jarisu) {
 		if(jarisu == n) {
-			if(isPrime(num)) {				
-				System.out.println(num);
-			}
+			System.out.println(num);
 			return;
 		}
 		
-		for(int i =1;i<10;i+=2) {
-			
-			int newnum = num*10 + i;
-			if(isPrime(newnum)) {
-				dfs(newnum,jarisu+1);
+		for(int i = 1;i<=9 ;i+=2) {
+			int fnum = num*10 + i;
+			if(isSosu(fnum)) {
+				dfs(fnum, jarisu + 1);
 			}
-			
 		}
-		
-		
+
 	}
-	
-	private static boolean isPrime(int num) 
-	{
-		for(int i =2;i*i<= num;i++) { // 
-			if(num % i == 0) {
-				
+
+
+	static boolean isSosu(int num) {
+		for(int i =2; i*i <=num ;i++ ) {
+			if(num % i ==0) {
 				return false;
 			}
-			
 		}
 		return true;
+		
 	}
 	
 }
