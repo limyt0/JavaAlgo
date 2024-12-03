cae775843416dba48d18de0c0e5df71b7c16a729
limyt0 <dudx45@gmail.com>
2024-12-03 19:38:56 +0900

    1920 수찾기 새로 풀이

--- JavaAlgo/a05/a5탐색29_p1920수찾기.java
+++ JavaAlgo/a05/a5탐색29_p1920수찾기.java
@@ -1,51 +1,51 @@
 package a05;
 
+
 import java.io.*;
 import java.util.*;
 
 public class a5탐색29_p1920수찾기 {
-	//static int _n;
-	//static int _nums[];
-	//static int _x;
-	//static int _xnums;
-	public static void main(String[] args) throws IOException {
+	
+	public static void main(String[] args) throws IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int _n = Integer.parseInt(st.nextToken());
-		int[] _a = new int[_n];
+		
+		int n = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0;i<_n;i++) {
-			_a[i] = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(_a);
+		
+		Arrays.sort(arr);
+		
 		st = new StringTokenizer(br.readLine());
-		int _m = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
-		for(int i =0; i<_m;i++) {
-			boolean find = false;
-			int target = Integer.parseInt(st.nextToken());
+		
+		
+
+		for(int i = 0;i<m;i++) {
+			int num = Integer.parseInt(st.nextToken());
+			int result = 0;
 			
-			//이진탐색 시작
-			int start = 0;
-			int end = _n - 1;
-			while(start <= end) {
-				int mid = (start+end)/2;
-				int midValue = _a[mid];
-				if(midValue > target) {
-					end = mid - 1;
-				}else if(midValue < target) {
-					start = mid + 1;
+			int s = 0;
+			int e = arr.length -1;
+			
+			while(s <=e) {
+				int mid = (s + e)/2;
+				int midVal = arr[mid];
+				if(midVal < num) {
+					s = mid+1;
+				}else if(midVal > num) {
+					e = mid-1;
 				}else {
-					find = true;
+					result = 1;
 					break;
 				}
 			}
-			if(find) {
-				System.out.println(1);
-			}else {
-				System.out.println(0);
-			}
 			
+			System.out.println(result);
 		}
 	}
 }
