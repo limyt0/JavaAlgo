8665b608281e843df2502e35bd065687c44081da
limyt0 <dudx45@gmail.com>
2024-12-07 12:28:43 +0900

    백준문제 기타레슨 재풀이

--- JavaAlgo/a05/a5탐색30_p2343기타레슨.java
+++ JavaAlgo/a05/a5탐색30_p2343기타레슨.java
@@ -7,40 +7,41 @@ public class a5탐색30_p2343기타레슨 {
 
 	public static void main(String[] args) throws IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new  StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		int[] arr = new int[n];
 		
+		int arr[] = new int[n];
+		st = new StringTokenizer(br.readLine());
 		int start = 0;
 		int end = 0;
 		
-		for(int i = 0;i<n;i++) {
+		for(int i=0;i<n;i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			if(start < arr[i]) start = arr[i];
-			end = end + arr[i];
+			if(start < arr[i]) {
+				start = arr[i];
+			}
+			end = end+ arr[i];
 		}
-		
-		while(start <= end) {
-			int middle = (start+end)/2;
-			int sum = 0;
+
+		while(start <=end) {
+			int mid = (start+end)/2;
 			int cnt = 0;
-			for(int i=0;i<n;i++) {
-				if(sum + arr[i] > middle) {
-					cnt++;
+			int sum = 0;
+			for(int i =0;i<n;i++) {
+				if(sum + arr[i] > mid) {
 					sum = 0;
+					cnt++;
 				}
-				sum = sum + arr[i];
-				
+				sum = sum+ arr[i];
 			}
 			if(sum !=0) {
 				cnt++;
 			}
-			if(cnt > m ) {
-				start = middle +1;
+			if(cnt > m) {
+				start = mid +1;
 			}else {
-				end = middle - 1;
+				end = mid -1;
 			}
 		}
 		System.out.println(start);
