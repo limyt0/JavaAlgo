7198da1cdfa5edb9c861e25c72fa32e41b7f3f28
limyt0 <dudx45@gmail.com>
2024-12-12 21:44:55 +0900

    그리디 동전문제

--- JavaAlgo/a06/a06그리디32_p11047동전0.java
+++ JavaAlgo/a06/a06그리디32_p11047동전0.java
@@ -8,22 +8,26 @@ public class a06그리디32_p11047동전0 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int _n = Integer.parseInt(st.nextToken());
-		int _k = Integer.parseInt(st.nextToken());
-		int[] _a = new int[_n];
-		
-		for(int i=0;i<_n;i++) {
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] coin = new int[n]; 
+		for(int i =0;i<n;i++) {
 			st = new StringTokenizer(br.readLine());
-			_a[i] = Integer.parseInt(st.nextToken());
+			coin[i] = Integer.parseInt(st.nextToken());
+		}
+		int cnt = 0;
+		for(int i=n-1;i>=0;i--) {
+			if(k > coin[i]) {
+				int mok = k / coin[i];
+				int nam = k % coin[i];
+				cnt += mok;
+				k = nam;
+				if(k == 0) {
+					break;
+				}
+			}
 		}
 		
-		int cnt=0;
-		for(int i = _n-1;i>=0;i--) {
-			if(_a[i] <= _k) {
-				cnt += (_k / _a[i]);
-				_k = _k % _a[i];
-			}			
-		}
 		System.out.println(cnt);
 	}
 
