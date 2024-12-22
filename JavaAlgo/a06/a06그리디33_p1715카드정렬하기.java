2513f1f3cc02c4afd2ba3aa8587227a7c4774376
limyt0 <dudx45@gmail.com>
2024-12-22 21:00:00 +0900

    카드 정렬하기 재풀이

--- JavaAlgo/a06/a06그리디33_p1715카드정렬하기.java
+++ JavaAlgo/a06/a06그리디33_p1715카드정렬하기.java
@@ -9,11 +9,8 @@ public class a06그리디33_p1715카드정렬하기 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-		
-		for(int i =0;i<n;i++) {
-		
+		for(int i = 0;i<n;i++) {
 			st = new StringTokenizer(br.readLine());
 			int data = Integer.parseInt(st.nextToken());
 			pq.add(data);
@@ -22,10 +19,10 @@ public class a06그리디33_p1715카드정렬하기 {
 		int data1 = 0;
 		int data2 = 0;
 		int sum = 0;
-		while(pq.size() > 1) {
+		while(pq.size()>1) {
 			data1 = pq.remove();
 			data2 = pq.remove();
-			sum += (data1 + data2);
+			sum += (data1 +data2);
 			pq.add(data1 + data2);
 		}
 		
