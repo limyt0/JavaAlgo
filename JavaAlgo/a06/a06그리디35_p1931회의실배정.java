f9e27fef21e965d8cb0542369263fb79fbe91f3c
limyt0 <dudx45@gmail.com>
2024-12-25 20:44:41 +0900

    회의실 배정 수정

--- JavaAlgo/a06/a06그리디35_p1931회의실배정.java
+++ JavaAlgo/a06/a06그리디35_p1931회의실배정.java
@@ -8,32 +8,33 @@ public class a06그리디35_p1931회의실배정 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int _n = Integer.parseInt(st.nextToken());
-		int[][] _a = new int[_n][2];
-		for(int i =0; i<_n;i++) {
+		int n = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[n][2];
+		for(int i = 0;i<n;i++) {
 			st = new StringTokenizer(br.readLine());
-			_a[i][0] = Integer.parseInt(st.nextToken());
-			_a[i][1] = Integer.parseInt(st.nextToken());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(_a, new Comparator<int[]>() {
+		Arrays.sort(arr, new Comparator<int[]>() {
 
 			@Override
-			public int compare(int[] S, int[] E) {
-				if(S[1] == E[1]) {					
-					return S[0]-E[0];
+			public int compare(int[] o1, int[] o2) {
+				if(o1[1] == o2[1]) {
+					return o1[0] - o2[0];
 				}
-				return S[1]-E[1];
+				return o1[1] - o2[1];
 			}
 		});
 		int cnt = 0;
-		int end = -1;
-		for(int i = 0; i<_n; i++) {
-			if(_a[i][0] >=end) {
-				end = _a[i][1];
+		int last = -1;
+		for(int i=0; i<n;i++) {
+			if(last <= arr[i][0]) {
+				last = arr[i][1];
 				cnt++;
-			}
+			} 
 		}
 		System.out.println(cnt);
+		
 	}
 
 }
