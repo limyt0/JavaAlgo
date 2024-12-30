82d1147ca499b722fd95a180b8bb0f3014a44a87
limyt0 <dudx45@gmail.com>
2024-12-30 22:22:50 +0900

    정수론 소수 구하기 수정

--- JavaAlgo/a07/a07정수론37_p1929소수구하기.java
+++ JavaAlgo/a07/a07정수론37_p1929소수구하기.java
@@ -8,24 +8,28 @@ public class a07정수론37_p1929소수구하기 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		boolean arr[] = new boolean[n+1];
+		for(int i=2;i<=n;i++) {
+			arr[i] = true;
+		}
 		
-		int _m = Integer.parseInt(st.nextToken());
-		int _n = Integer.parseInt(st.nextToken());
-		int[] _a = new int[_n+1];
-		for(int i =2; i<=_n;i++) {
-			_a[i] = i;
-		}
-		for(int i=2;i<=Math.sqrt(_n);i++) {
-			if(_a[i] == 0) {
+		for(int start = 2;start*start<=n;start++) {
+			if(!arr[start]) {
 				continue;
+				
 			}
-			for(int j= i+i;j<=_n;j=j+i) {
-				_a[j] = 0;
+			for(int i = start+start;i<=n;i+=start) {
+				//if(i % start == 0 && i != start) {
+			//	}
+				arr[i] = false;
 			}
 		}
-		for(int i =_m; i<=_n;i++) {
-			if(_a[i] !=0) {
-				System.out.println(_a[i]);
+		
+		for(int i = m;i<=n;i++) {
+			if(arr[i]) {
+				System.out.println(i);				
 			}
 		}
 	}
