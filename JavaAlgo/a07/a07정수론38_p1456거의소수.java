9ff72f430095b8f5b70e34dfd741a6b32683d7cc
limyt0 <dudx45@gmail.com>
2025-01-01 21:57:48 +0900

    거의 소수 수정

--- JavaAlgo/a07/a07정수론38_p1456거의소수.java
+++ JavaAlgo/a07/a07정수론38_p1456거의소수.java
@@ -1,35 +1,47 @@
 package a07;
 
 import java.util.*;
+import java.io.*;
 public class a07정수론38_p1456거의소수 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		long Min = sc.nextLong();
-		long Max = sc.nextLong();
-		long[] A = new long[10000001];
-		for(int i =2; i<A.length;i++) {
-			A[i] = i;
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		
+		int alength = 10000000;
+		if(b <= alength ) {
+			alength = (int)b;
 		}
-		for(int i =2;i<=Math.sqrt(A.length);i++) {
-			if(A[i] ==0) {
+		boolean arr[] = new boolean[alength+1];
+		//int alength = arr.length;
+		for(int i = 2;i<alength;i++) {
+			arr[i] = true;
+		}
+		
+		for(int start = 2; start*start <=alength;start++ ) {
+			if(!arr[start]) {
 				continue;
 			}
-			for(int j = i+i;j<A.length;j=j+i) {
-				A[j]=0;
+			for(int i =start + start;i<alength;i+=start) {
+				if(arr[i]){
+					arr[i] = false;
+				}
 			}
 		}
 		int cnt = 0;
-		for(int i =2;i<10000001;i++) {
-			if(A[i]!=0) {
-				long temp = A[i];
-				while((double)A[i] <= (double)Max/(double)temp) {
-					if((double)A[i] >= (double)Min/(double)temp) {
-						cnt++;
+		for(int i = 2;i<=alength;i++) {
+			if(arr[i]) {
+				long temp = i;
+				while((double)i <= (double)b/(double)temp) {
+					if((double)i>=(double)a/(double)temp) {
+						cnt++;						
 					}
-					temp = temp*A[i];
+					temp *= i;
 				}
 			}
 		}
 		System.out.println(cnt);
+		
+		
 	}
 }
