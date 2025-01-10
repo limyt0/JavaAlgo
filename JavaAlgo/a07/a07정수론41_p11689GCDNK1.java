20eef475ab11385b9271cfadfea956c99f641797
limyt0 <dudx45@gmail.com>
2025-01-10 20:50:19 +0900

    GCDNK수정

--- JavaAlgo/a07/a07정수론41_p11689GCDNK1.java
+++ JavaAlgo/a07/a07정수론41_p11689GCDNK1.java
@@ -5,23 +5,26 @@ import java.util.*;
 
 public class a07정수론41_p11689GCDNK1 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		long n = Long.parseLong(st.nextToken());
 		long result = n;
-		for(long p=2; p<=Math.sqrt(n); p++) {
-			if(n%p==0) {
-				result = result - result/p;
-				while(n%p == 0) {
-					n /= p;
+		for(long i = 2;i*i<=n;i++) {
+			if(n % i == 0) {
+				result = result - result / i;
+				while(n % i ==0) {
+					n /= i;
 				}
 			}
 		}
 		if(n>1) {
-			result = result - result /n;
+			result = result - result / n;
 		}
 		System.out.println(result);
+		
 	}
+
+
 
 }
