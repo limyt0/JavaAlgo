027841f670637782f962997a5b1a0f598344c195
limyt0 <dudx45@gmail.com>
2025-01-19 18:35:09 +0900

    칵테일 수정

--- JavaAlgo/a07/a07정수론44_p1033_칵테일.java
+++ JavaAlgo/a07/a07정수론44_p1033_칵테일.java
@@ -4,82 +4,98 @@ import java.io.*;
 import java.util.*;
 
 public class a07정수론44_p1033_칵테일 {
-	//static int n;//재료 수
-	static ArrayList<cNode>[] alist;//인접리스트
-	static long lcm; //최소공배수
-	static long[] d;//각 노드 저장 배열
-	static boolean[] visited;//방문여부 저장 배열
+
+	static ArrayList<Cnodes>[] arr; 
+	static boolean visited[];
+	static int n;
+	static long biglcm;
+	static long d[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		
-		alist = new ArrayList[n];
+
+		n = Integer.parseInt(st.nextToken());
 		visited = new boolean[n];
+		arr = new ArrayList[n];
+		biglcm = 1;
 		d = new long[n];
-		lcm = 1;
-		for(int i=0;i<n;i++) {
-			alist[i] = new ArrayList<cNode>();			
+		for(int i = 0;i<n;i++) {
+			arr[i] = new ArrayList<Cnodes>();
 		}
-		
-		for(int i = 0;i<n-1;i++) {
+		for(int i =0;i<n-1;i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int p = Integer.parseInt(st.nextToken());
 			int q = Integer.parseInt(st.nextToken());
-			alist[a].add(new cNode(b,p,q));
-			alist[b].add(new cNode(a, p, q));			
-			lcm *= (p*q / gcd(p,q)); 
+			arr[a].add(new Cnodes(b, p, q));
+			arr[b].add(new Cnodes(a, q, p));
+			biglcm *= (p * q / gcd(p, q));
 		}
-		d[0] = lcm;
-		DFS(0);
+		d[0] = biglcm;
+		dfs(0);
 		long mgcd = d[0];
 		for(int i = 1;i<n;i++) {
 			mgcd = gcd(mgcd, d[i]);
 		}
+		for(int i = 0;i<n;i++) {
+			System.out.print(d[i]/ mgcd+ " ");
+		}
 		
-		StringBuilder sb = new StringBuilder("");
-		for(int i =0;i<n;i++) {
-			long result = (d[i] / mgcd);
-			//System.out.println(sb);
-			//sb.append(d[i]+"/"+mgcd+"="+ result + "\n");
-		}
-		//System.out.println(sb);
-	}
-	private static void DFS(int node) {
-		visited[node] = true;
-		for( cNode i:alist[node]) {
-			int next = i.getB();
-			if(!visited[next]) {
-				d[next] = d[node] * i.getQ() / i.getP();
-				DFS(next);
-			}
-		}
+		//System.out.println("result"+lcm);
 	}
 	private static long gcd(long a, long b) {
-		if(b ==0)
+		if(b == 0) {
 			return a;
-		else
-			return gcd(b, a % b);
+		}else {
+			return gcd(b, a%b);
+		}
+		
 	}
 	
+	
+	
+	private static void dfs(int node) {
+		visited[node] = true;
+		for(int i = 0;i<arr[node].size();i++) {
+			int b = arr[node].get(i).b;
+			int p = arr[node].get(i).p;
+			int q = arr[node].get(i).q;
+			//System.out.println(b+", "+p+", " + q);
+			if(!visited[b]) {
+				d[b] = (d[node] * q) / p; 
+				dfs(b);
+			}
+		}
+	
+//		for(Cnodes i : arr[node]) {
+//			int next = i.b;
+//			if(!visited[next]) {
+//				d[next] = d[node] * i.q / i.p;
+//				dfs(next);
+//			}
+//		}
+	}
+
+
+
+
 	
 	
 	
 }
- class cNode{
-	int b;
-	int p;
-	int q;
-	public cNode(int b, int p, int q) {
-		super();
+
+class Cnodes{
+	public int b;
+    public int p;
+	public int q;
+	public Cnodes(int b, int p, int q){
 		this.b = b;
 		this.p = p;
-		this.q = q;			
+		this.q = q;
 	}
 	public int getB() {
-		return b;			
+		return b;
 	}
 	public int getP() {
 		return p;
