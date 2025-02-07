c7da32332f6b90d634b6ce07d600f81d90c1de25
limyt0 <dudx45@gmail.com>
2025-02-07 17:56:20 +0900

    특정거리의 도시 찾기 수정

--- JavaAlgo/a08/a08그래프46_p18352특정거리의도시찾기.java
+++ JavaAlgo/a08/a08그래프46_p18352특정거리의도시찾기.java
@@ -1,52 +1,71 @@
 package a08;
 
-import java.io.*;
 import java.util.*;
 
 public class a08그래프46_p18352특정거리의도시찾기 {
-	//static int[][] marray;//단방향 저장 array
-	static ArrayList<Integer>[] mlist;//단반향 저장 array
-	static boolean[] visited;
-	static int n,m,k,x;
-	static ArrayList<Integer> nlist; //k길이를 갖는 도시들을 저장하는 array
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m =Integer.parseInt(st.nextToken());
-		k =Integer.parseInt(st.nextToken());
-		x = Integer.parseInt(st.nextToken());
-		//marray = new int[2][m] ;
-		visited = new boolean[m];
-		mlist = new ArrayList[n+1];
+	static ArrayList<Integer>[] arr;
+	static int[] visited;
+	static List<Integer> answer;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int k = sc.nextInt();
+		int x = sc.nextInt();
+		
+		arr= new ArrayList[n+1];
 		
 		for(int i = 0;i<=n;i++) {
-			mlist[i] = new ArrayList<Integer>();
+			arr[i] = new ArrayList<Integer>();
 		}
 		
-		for(int i = 1; i<=m;i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-		    mlist[s].add(e);
+		for(int i = 0;i<m;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			arr[a].add(b);
 		}
-		nlist = new ArrayList<Integer>();
-	   lowdist(1, 0);
-	   
+		visited = new int[n+1];
+		for(int i = 0;i<=n;i++) {
+			visited[i] = -1;
+		}
+		bfs(x);
+		answer = new ArrayList<Integer>();
+		for(int i=0;i<=n;i++) {
+			if(visited[i] == k) {
+				answer.add(i);
+			}
+		}
+		if(answer.isEmpty()) {
+			System.out.println("-1");
+		}else {
+			Collections.sort(answer);
+			for(int an : answer) {
+				System.out.println(an);
+			}
+		}
+		
+		
+	}
+	private static void bfs(int index) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(index);
+		visited[index]++;
+		while(!queue.isEmpty()) {
+			int now = queue.poll();
+			for(int node : arr[now]) {
+				if(visited[node] == -1) {
+					visited[node] = visited[now] +1;
+					queue.add(node);
+				}
+			}
+		}
 	}
 	
-	//최단 거리일때 nlist에 값을 넣기
-	private static void lowdist(int num, int cnt) {
-		if(cnt == k) {
-			nlist.add(num);
-			return;
-	    }
-	    
-		for(int i = num; i <=n ;i++) {			
-			
-		}
-	    	
-	       
-	    		
-	}
+	
+	
+	   
+	
+
+	
+	
 }
