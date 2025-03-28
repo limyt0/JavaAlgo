6d1d8649c8ecca4abfacb1c0dfdf10582ad19ec0
limyt0 <dudx45@gmail.com>
2025-03-28 09:25:55 +0900

    정렬 15번 풀이

---
+++ JavaAlgo/a04/a04정렬15_p2750수정렬하기.java
@@ -0,0 +1,31 @@
+package a04;
+
+import java.util.*;
+
+public class a04정렬15_p2750수정렬하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr= new int[N];
+		for(int i = 0;i<N;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i = 0;i<N-1;i++) {
+			for(int j = 0;j<N-1-i;j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j+1];
+					arr[j+1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+		
+		for(int i = 0;i<N;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
