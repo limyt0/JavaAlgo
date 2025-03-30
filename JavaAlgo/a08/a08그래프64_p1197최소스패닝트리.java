da61d0ed20ce69e564ffd81afa3341e09d0cdfd4
limyt0 <dudx45@gmail.com>
2025-03-31 07:47:53 +0900

    그래프 64번

---
+++ JavaAlgo/a08/a08그래프64_p1197최소스패닝트리.java
@@ -0,0 +1,21 @@
+package a08;
+
+import java.io.*;
+import java.util.*;
+
+public class a08그래프64_p1197최소스패닝트리 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		for(int i = 0;i<E;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a= Integer.parseInt(st.nextToken());
+			int b= Integer.parseInt(st.nextToken());
+			int c= Integer.parseInt(st.nextToken());
+		}
+	}
+
+}
