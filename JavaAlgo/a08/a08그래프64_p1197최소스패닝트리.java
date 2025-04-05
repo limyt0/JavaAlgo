43bf11e16d674a81e728672072823d45dd22e9a3
limyt0 <dudx45@gmail.com>
2025-04-05 10:40:35 +0900

    그래프 64 풀이

--- JavaAlgo/a08/a08그래프64_p1197최소스패닝트리.java
+++ JavaAlgo/a08/a08그래프64_p1197최소스패닝트리.java
@@ -4,18 +4,70 @@ import java.io.*;
 import java.util.*;
 
 public class a08그래프64_p1197최소스패닝트리 {
-
+	static int[] parent;
+	static PriorityQueue<pEdge> queue;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int V = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
+		queue = new PriorityQueue<>();
+		parent = new int[V+1];
+		for(int i=0;i<V;i++) {
+			parent[i] = i;
+		}
 		for(int i = 0;i<E;i++) {
 			st = new StringTokenizer(br.readLine());
 			int a= Integer.parseInt(st.nextToken());
 			int b= Integer.parseInt(st.nextToken());
 			int c= Integer.parseInt(st.nextToken());
+			queue.add(new pEdge(a, b, c));
+		}
+		
+		int useEdge = 0;
+		int result = 0;
+		
+		while(useEdge < V - 1) {
+			pEdge now = queue.poll();
+			if(find(now.s) != find(now.e)) {
+				union(now.s, now.e);
+				result = result + now.v;
+				useEdge++;
+			}
+		}
+		System.out.println(result);
+	}
+	private static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if(a!=b) {
+			parent[b] = a;
+		}
+		
+	}
+	private static int find(int a) {
+		if(a == parent[a]) {
+			return a;
+		}else {
+			
+			return parent[a] = find(parent[a]);
 		}
 	}
+	
+}
 
+class pEdge implements Comparable<pEdge>{
+	int s;
+	int e;
+	int v;
+	pEdge(int s, int e, int v){
+		
+		this.s = s;
+		this.e = e;
+		this.v = v;
+	}
+	@Override
+	public int compareTo(pEdge o) {
+		return this.v - o.v;
+	}
 }
