166d17b5a4d8f56356b4fa6c37e6346f725810b2
limyt0 <dudx45@gmail.com>
2025-05-06 09:19:22 +0900

    트리 71번 푸는 중

---
+++ JavaAlgo/a09/a09트리71_p2042구간합구하기.java
@@ -0,0 +1,16 @@
+package a09;
+
+import java.io.*;
+import java.util.*;
+
+public class a09트리71_p2042구간합구하기 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+	}
+
+}
